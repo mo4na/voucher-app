67e2cc5610acf29475eab3912aeba44e200fe46f
Kumar Wanget <2226699@slu.edu.ph>
2026-03-05 15:21:31 +0800

    index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 require_once __DIR__ . '/../app/lib/auth.php';
-if (current_user()) header("Location: dashboard.php");
-else header("Location: login.php");
+if (current_user()) {
+  header('Location: dashboard.php');
+  exit;
+}
+header('Location: login.php');
 exit;
